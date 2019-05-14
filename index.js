38cd4d5c2bcd8313038e09ba6ad7831d2c88d830
Sho Ishikura <shoishikura@gmail.com>
2019-05-14 16:38:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,5 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase === string);
   return ("I can't hear you!") 
 } else {
-  if(string.toUpperCase === string);
   return ("YES INDEED!") 
 }
