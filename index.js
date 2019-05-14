231f7905ec2b1f85af2b86707e29eb0a77d8a55d
Sho Ishikura <shoishikura@gmail.com>
2019-05-14 16:09:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+'Hello!'.toUpperCase() // 'HELLO!'
+function shout(string) {
+  return string.toUpperCase()
+}
+
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase
+mixedCase.toUpperCase() === mixedCase
+
+'Hello!'.toLowerCase() // 'hello!'
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log (string.toUpperCase() )
+}
+function logWhisper(string) {
+  console.log (string.toLowerCase() )
+}
+function sayHiToGrandma(string) {
+  return ("I can't hear you!") 
+  if ("sayHiToGrandma") ; lowercase
+}
+function sayHiToGrandma(string) {
+  return ("YES INDEED!") 
+  if ("sayHiToGrandma") ; uppercase
+}
